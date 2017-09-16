6890f5a44db38fe6020197e47d197815cee1da9a
parkito <artyom-karnov@yandex.ru>
2017-09-16 16:36:03 +0300

    Add web module

--- src/main/java/com/karnov/crud/controller/UserRegistrationController.java
+++ src/main/java/com/karnov/crud/controller/UserRegistrationController.java
@@ -1,8 +1,23 @@
 package com.karnov.crud.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author Artem Karnov @date 15.09.2017.
  * artem.karnov@t-systems.com
  */
-public class UserRegistrationController {
+public class UserRegistrationController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
