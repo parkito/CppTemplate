f07b8b5a53a18fde23df8f613841168d46960fa0
parkito <artyom-karnov@yandex.ru>
2017-09-23 18:20:38 +0300

    Add code for user registration

--- src/main/java/com/karnov/crud/controller/UserRegistrationController.java
+++ src/main/java/com/karnov/crud/controller/UserRegistrationController.java
@@ -1,9 +1,7 @@
 package com.karnov.crud.controller;
 
-import com.karnov.crud.entity.Person;
 import com.karnov.crud.service.PersonService;
 import com.karnov.crud.utility.DIContainer;
-import com.karnov.crud.utility.Utility;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,17 +18,13 @@ public class UserRegistrationController extends HttpServlet {
     private PersonService personService = DIContainer.userServiceInstance();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Person person = new Person();
-        person.setEmail("email1");
-        person.setFirstName("firstName1");
-        person.setLastName("lastName1");
-        person.setLearningGroup("learningGroup1");
-        person.setPassword(Utility.encryptPassword("password1"));
-        personService.createPerson(person);
+        response.sendRedirect("/location.jsp");
     }
 
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println(username + " " + password);
     }
 }
