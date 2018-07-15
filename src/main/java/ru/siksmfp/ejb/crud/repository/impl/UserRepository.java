4efd01c94a6e1e564c18762205a9478334c02c13
parkito <artyom-karnov@yandex.ru>
2018-07-15 03:10:45 +0300

    Fix deps versions

--- src/main/java/ru/siksmfp/ejb/crud/repository/impl/UserRepository.java
+++ src/main/java/ru/siksmfp/ejb/crud/repository/impl/UserRepository.java
@@ -26,19 +26,19 @@ public class UserRepository extends GenericRepository<UserEntity, Long> {
 
     @PostConstruct
     public void setUp() {
-        deleteAll();
-
-        UserEntity user1 = new UserEntity("email1", "name1", "secondName1", "1", Role.ADMIN);
-        UserEntity user2 = new UserEntity("email2", "name2", "secondName2", "2", Role.USER);
-        UserEntity user3 = new UserEntity("email3", "name3", "secondName3", "3", Role.USER);
-        UserEntity user4 = new UserEntity("email4", "name4", "secondName4", "4", Role.ROOT);
-        UserEntity user5 = new UserEntity("email5", "name5", "secondName5", "5", Role.USER);
-        UserEntity user6 = new UserEntity("email6", "name6", "secondName6", "6", Role.USER);
-        UserEntity user7 = new UserEntity("email7", "name7", "secondName7", "7", Role.USER);
-        UserEntity user8 = new UserEntity("email8", "name8", "secondName8", "8", Role.ADMIN);
-
-        List<UserEntity> userEntities = Arrays.asList(user1, user2, user3, user4, user5, user6, user7, user8);
-        batchSave(userEntities);
+//        deleteAll();
+//
+//        UserEntity user1 = new UserEntity("email1", "name1", "secondName1", "1", Role.ADMIN);
+//        UserEntity user2 = new UserEntity("email2", "name2", "secondName2", "2", Role.USER);
+//        UserEntity user3 = new UserEntity("email3", "name3", "secondName3", "3", Role.USER);
+//        UserEntity user4 = new UserEntity("email4", "name4", "secondName4", "4", Role.ROOT);
+//        UserEntity user5 = new UserEntity("email5", "name5", "secondName5", "5", Role.USER);
+//        UserEntity user6 = new UserEntity("email6", "name6", "secondName6", "6", Role.USER);
+//        UserEntity user7 = new UserEntity("email7", "name7", "secondName7", "7", Role.USER);
+//        UserEntity user8 = new UserEntity("email8", "name8", "secondName8", "8", Role.ADMIN);
+//
+//        List<UserEntity> userEntities = Arrays.asList(user1, user2, user3, user4, user5, user6, user7, user8);
+//        batchSave(userEntities);
     }
 
     public UserEntity findUserByEmail(String email) {
