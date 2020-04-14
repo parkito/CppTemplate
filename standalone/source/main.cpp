09bcada881bd5c44953d4593c2f8f8b96b7df81c
Lars Melchior <TheLartians@users.noreply.github.com>
2020-04-14 13:59:38 +0200

    Add optional standalone executable directory (#2)
    * add standalone executable
    
    * update readme
    
    * add standalone workflow
    
    * fix path

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,8 +1,8 @@
-#include <greeter/greeter.h>
-#include <greeter/version.h>
+#include <greeter.h>
 
 #include <cxxopts.hpp>
 #include <iostream>
+#include <string>
 #include <unordered_map>
 
 const std::unordered_map<std::string, greeter::LanguageCode> languages{
@@ -21,7 +21,6 @@ int main(int argc, char** argv) {
   // clang-format off
   options.add_options()
     ("h,help", "Show help")
-    ("v,version", "Print the current version number")
     ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
     ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
   ;
@@ -32,14 +31,11 @@ int main(int argc, char** argv) {
   if (result["help"].as<bool>()) {
     std::cout << options.help() << std::endl;
     return 0;
-  } else if (result["version"].as<bool>()) {
-    std::cout << "Greeter, version " << GREETER_VERSION << std::endl;
-    return 0;
   }
 
   auto langIt = languages.find(language);
   if (langIt == languages.end()) {
-    std::cerr << "unknown language code: " << language << std::endl;
+    std::cout << "unknown language code: " << language << std::endl;
     return 1;
   }
 
