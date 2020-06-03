c84bf4c467780d69f5af95dcf129edbbf121bf87
Lars Melchior <TheLartians@users.noreply.github.com>
2020-06-03 16:25:44 +0200

    Create version header and update readme (#44)
    * add note on github pages
    
    * add version information to standalone. closes #35.
    
    * update version info
    
    * move includes into project directory and create version header
    
    * fix style
    
    * add comment for version header location

--- include/greeter/greeter.h
+++ include/greeter/greeter.h
@@ -8,20 +8,20 @@ namespace greeter {
   enum class LanguageCode { EN, DE, ES, FR };
 
   /**
-   * @brief A class for saying hello in multiple languages
+   * A class for saying hello in multiple languages
    */
   class Greeter {
     std::string name;
 
   public:
     /**
-     * @brief Creates a new greeter
+     * Creates a new greeter
      * @param name the name to greet
      */
     Greeter(std::string name);
 
     /**
-     * @brief Creates a localized string containing the greeting
+     * Creates a localized string containing the greeting
      * @param lang the language to greet in
      * @return a string containing the greeting
      */
